46589527bf348d9875db441cbb90848bad4a0f0e
Wafarm <qinyy07@gmail.com>
2023-07-31 11:11:29 +0800

    chore: add icon and rename

--- src/main/java/io/github/wafarm/calculator/CalculatorClient.java
+++ src/main/java/io/github/wafarm/calculator/CalculatorClient.java
@@ -7,15 +7,10 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallba
 import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
 import net.minecraft.command.argument.serialize.ConstantArgumentSerializer;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CalculatorClient implements ClientModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("calculator");
-
     @Override
     public void onInitializeClient() {
-        LOGGER.info("Loading Calculator Client...");
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             CalculateCommand.register(dispatcher);
         });
